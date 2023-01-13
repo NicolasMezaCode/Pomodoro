f56afac1bbb186738d84677531f60dd9aeec4fdc
Nicolas <nickismeza@gmail.com>
2023-01-12 16:14:47 -0800

    fixing header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,19 +6,21 @@ import {useColor} from '../context/ColorContext'
 export default function Header() {
   const {color} = useColor();
   return (
-    <header style={{backgroundColor:`${color.main}`,}}>
-      <div>
-        <h1>PomoTimer</h1>
-        <RiTimerFlashFill />
+    <header className='flex justify-center bg-transparent max-h-20'>
+      <div className='w-screen flex justify-between items-center flex-row p-2 ' style={{backgroundColor:`${color.main}`,}}>
+        <div className='flex justify-center items-center'>
+          <RiTimerFlashFill className='h-auto w-9 ' />
+          <h1 className='font-semibold ml-1 sm:text-4xl'>PomoTimer</h1>
+        </div>
+        <button className='flex justify-center items-center ' style={{backgroundColor:`${color.main}`,}}>
+          <IoMdSettings />
+          <span>Settings</span>
+        </button>
+        <button className='flex justify-center items-center ' style={{backgroundColor:`${color.main}`,}} >
+          <AiFillInfoCircle />
+          <span>Info</span>
+        </button>
       </div>
-      <button style={{backgroundColor:`${color.main}`,}}>
-        <IoMdSettings />
-        <span>Settings</span>
-      </button>
-      <button style={{backgroundColor:`${color.main}`,}} >
-        <AiFillInfoCircle />
-        <span>Info</span>
-      </button>
     </header>
   )
 }
